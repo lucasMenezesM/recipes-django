4475624e08dff45a4e0715d7f71d12febca1d353
Lucas Menezes <livedolux@gmail.com>
2025-12-03 11:04:18 -0300

    add recipes app

---
+++ recipes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from recipes import views
+
+urlpatterns = [
+    path('', views.home),
+    path('contact/', views.contact),
+    path('about/', views.about)
+]
---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def contact(request):
+    return HttpResponse('contact page')
+
+
+def about(request):
+    return HttpResponse('About page')
