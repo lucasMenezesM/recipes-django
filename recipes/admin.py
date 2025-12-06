6f2606b112c3c60d523dd2e84bb8acc1cd876de6
Lucas Menezes <livedolux@gmail.com>
2025-12-06 11:13:13 -0300

    feat: add media handling and enhance recipe model with additional fields

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Recipe, Category
 
 # Register your models here.
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
