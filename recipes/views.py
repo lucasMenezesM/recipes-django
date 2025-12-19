0e6fdfd04d952d8f5a9133e577caae00de79919f
Lucas Menezes <livedolux@gmail.com>
2025-12-19 10:44:36 -0300

    refactor: update Recipe model to allow optional cover image; enhance recipe item template for dynamic cover display

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from utils.recipes.factory import make_recipe
-from .models import User, Recipe, Category
+from .models import Recipe
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True)
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': Recipe.objects.all()
+        'recipes': recipes
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category_id=category_id, is_published=True)
+    return render(request, 'recipes/pages/home.html', context = {
+        'recipes': recipes
     })
 
 
 def recipe(request, id):
+    recipe_item = Recipe.objects.filter(id=id, is_published=True).first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': Recipe.objects.filter(id=id).first(),
+        'recipe': recipe_item,
         'is_detail_page': True
     })
+
