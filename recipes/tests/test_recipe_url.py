85da9696eea9220fb4d0a40af73d38c5a8df9937
Lucas Menezes <livedolux@gmail.com>
2025-12-21 16:59:43 -0300

    refactor: create new structure for tests

---
+++ recipes/tests/test_recipe_url.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RecipeURLsTest(TestCase):
+
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        assert url == '/'
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        assert url == '/recipes/categories/1/'
+
+    def test_recipe_details_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        assert url == '/recipes/1/'
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,23 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from . import views
-
-# Create your tests here.
-
-
-class RecipeURLsTest(TestCase):
-
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        assert url == '/'
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        assert url == '/recipes/categories/1/'
-
-    def test_recipe_details_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        assert url == '/recipes/1/'
+from .. import views
 
 
 class RecipeViewsTest(TestCase):
