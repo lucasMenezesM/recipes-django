9ace9230f9b99cb9c492917fb9709ef9e33d0967
Lucas Menezes <livedolux@gmail.com>
2025-12-22 12:06:28 -0300

    refactor: migrate utility functions to BaseViewTest and remove utils.py

---
+++ recipes/tests/base_view_test.py
@@ -0,0 +1,71 @@
+
+from django.urls import reverse
+from ..models import Category, Recipe, User
+from django.test import TestCase
+
+
+class BaseViewTest(TestCase):
+
+    def setUp(self):
+        return super().setUp()
+
+    def home_url(self):
+        return reverse('recipes:home')
+
+    def category_url(self, id: int = 1):
+        return reverse('recipes:category',  kwargs={'category_id': id})
+
+    def recipe_url(self, id: int = 1):
+        return reverse('recipes:recipe', kwargs={'id': id})
+
+    def get_category(self, name='Category 1'):
+        return Category.objects.create(name=name)
+
+    def get_user(self, first_name='User',
+                 last_name='User',
+                 username='username',
+                 email='username@email.com',
+                 password='123456',
+                 ):
+
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password
+        )
+
+    def get_recipe(self, title='Recipe',
+                   description='Pretty good Recipe',
+                   slug='recipe-recipe',
+                   preparation_time=2,
+                   preparation_time_unit='minutes',
+                   servings=2,
+                   servings_unit='people',
+                   preparation_steps=2,
+                   preparation_steps_is_html=True,
+                   category=None,
+                   author=None,
+                   is_published=True
+                   ):
+
+        if category is None:
+            category = {}
+        if author is None:
+            author = {}
+
+        return Recipe.objects.create(
+            title=title,
+            description=description,
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit=preparation_time_unit,
+            servings=servings,
+            servings_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparation_steps_is_html=preparation_steps_is_html,
+            category=self.get_category(**category),
+            author=self.get_user(**author),
+            is_published=True
+        )
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,70 +1,69 @@
-from django.test import TestCase
 from django.urls import resolve
 from recipes import views
 # from ..models import Category, Recipe, User
-from .utils import get_category, get_recipe, home_url, category_url, recipe_url
+from .base_view_test import BaseViewTest
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(BaseViewTest):
 
     # View functions tests
 
     def test_home_view_function(self):
-        view = resolve(home_url())
+        view = resolve(self.home_url())
         assert view.func is views.home
 
     def test_category_view_function(self):
-        view = resolve(category_url())
+        view = resolve(self.category_url())
         assert view.func is views.category
 
     def test_recipe_view_function(self):
-        view = resolve(recipe_url())
+        view = resolve(self.recipe_url())
         assert view.func == views.recipe
 
     # Status Code 200 tests
 
     def test_home_view_returns_status_code_200_ok(self):
-        response = self.client.get(home_url())
+        response = self.client.get(self.home_url())
         assert response.status_code == 200
 
     def test_category_view_returns_status_code_200_ok(self):
-        category = get_category()
-        response = self.client.get(category_url(id=category.id))
+        category = self.get_category()
+        response = self.client.get(self.category_url(id=category.id))
         assert response.status_code == 200
 
     def test_recipe_view_returns_status_code_200_ok(self):
-        recipe = get_recipe()
-        response = self.client.get(recipe_url(id=recipe.id))
+        recipe = self.get_recipe()
+        response = self.client.get(self.recipe_url(id=recipe.id))
         assert response.status_code == 200
 
     # Status code 404 tests
 
     def test_category_view_returns_status_code_404_ok(self):
-        response = self.client.get(category_url(id=1000))
+        response = self.client.get(self.category_url(id=1000))
         assert response.status_code == 404
 
     def test_recipe_view_returns_status_code_404_ok(self):
-        response = self.client.get(recipe_url(id=10000))
+        response = self.client.get(self.recipe_url(id=10000))
         assert response.status_code == 404
 
     def test_recipe_template_shows_not_found_message(self):
-        response = self.client.get(home_url())
+        response = self.client.get(self.home_url())
         content = response.content.decode('utf-8')
         assert 'No recipes found here' in content
 
-    # Templates tests
+    # Loading Correct Template Tests
 
     def test_home_view_loads_correct_template(self):
-        response = self.client.get(home_url())
+        response = self.client.get(self.home_url())
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_view_loads_correct_template(self):
-        recipe = get_recipe()
-        response = self.client.get(recipe_url(id=recipe.id))
+        recipe = self.get_recipe()
+        response = self.client.get(self.recipe_url(id=recipe.id))
         self.assertTemplateUsed(response, 'recipes/pages/recipe-view.html')
 
     def test_category_view_loads_correct_template(self):
-        category = get_category()
-        response = self.client.get(category_url(id=category.id))
+        category = self.get_category()
+        response = self.client.get(self.category_url(id=category.id))
         self.assertTemplateUsed(
             response, 'recipes/pages/recipes-category.html')
