3248871d01273f5375ceea0e08ead021d39ced97
Lucas Menezes <livedolux@gmail.com>
2026-01-06 18:33:29 -0300

    feat: implement search functionality with error handling and update templates

--- recipes/tests/View/test_search_view.py
+++ recipes/tests/View/test_search_view.py
@@ -10,5 +10,16 @@ class TestRecipeView(BaseRecipesTest):
         assert view.func == search
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(self.search_url())
+        response = self.client.get(self.search_url() + "?q=test")
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        response = self.client.get(self.search_url())
+
+        assert response.status_code == 404
+
+    def test_recipe_search_term_is_on_page(self):
+        response = self.client.get(self.search_url() + "?q=test")
+        content = response.content.decode('utf-8')
+
+        assert 'Searching for "test"' in content
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe, Category
+from django.http import Http404
 # Create your views here.
 
 
@@ -32,4 +33,11 @@ def recipe(request, id):
 
 
 def search(request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html', context={
+        "search_term": search_term
+    })
